4f2f8fd48d99a684ecc42ec74994044c7a8fa389
lucianefalcao <lucianefalcao09@gmail.com>
2019-03-26 18:45:57 -0300

    Adicionado opção de escolha(switch-case)

--- expressoes_regulares.cpp
+++ expressoes_regulares.cpp
@@ -9,8 +9,12 @@ using namespace std;
  * Se não for válido, escreve a mensagem CPF não válido **/
 void CPFValidacao(string str){
 
+    cout << "Digite o CPF" << endl;
+
+    cin >> str;
+
     // Especifica o padrão do CPF 3 dígitos, ponto, 3 dígitos, hífen, 2 dígitos 
-    regex cpf("[[:d:]]{3} . [[:d:]]{3} - [[:d:]]{2}");
+    regex cpf("[[:d:]]{3}.[[:d:]]{3}-[[:d:]]{2}");
 
     //Verifica se o CPF passado é válido
     if(regex_match(str, cpf)){
@@ -25,6 +29,10 @@ void CPFValidacao(string str){
  * Se for válido, escreve a mensagem Telefone Válido
  * Se não for válido, escreve a mensagem telefone não válido **/
 void NumeroTel(string str){
+
+    cout << "Digite o telefone" << endl;
+    
+    cin >> str;
 
     // Especifica o padrão do telefone 4 ou 5 dígitos, hífen, 4 dígitos
     regex num("[[:d:]]{4,5} - [[:d:]]{4}");
@@ -42,6 +50,10 @@ void NumeroTel(string str){
  * Se for válido, escreve a mensagem Email Válido
  * Se não for válido, escreve a mensagem Email não válido **/
 void EmailValido(string str){
+
+    cout << "Digite o email" << endl;
+    
+    cin >> str;
 
     // Especifica o padrão do email 1 ou mais dígitos, "@", domínio, ".com", ".br" ou ".org"
     regex email("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(com|br|org))");
@@ -61,6 +73,10 @@ void EmailValido(string str){
  * Se não for válido, escreve a mensagem Email não válido **/
 void URLValida(string str){
 
+    cout << "Digite a URL" << endl;
+    
+    cin >> str;
+
     // Especifica o padrão de uma url: wwww, ponto, domínio, ".com", ".br" ou ".org"
     regex url("(w){3}(\\.)(\\w*)(\\.(com|br|org))");
     
@@ -77,16 +93,36 @@ void URLValida(string str){
 int main(){
 
     string str;
+    int caso;
 
-    while(true){
+    cout << "Escolha o que você deseja validar" << endl << "Opção 1: CPF" << endl
+        << "Opção 2: Número de Telefone" << endl << "Opção 3: Email" << endl << "Opção 4: URl" << endl;
+     
 
-        cin >> str;
+        cin >> caso;
 
+        switch (caso)
+        {
+            case 1:
+                CPFValidacao(str);
+                break;
+            case 2:
+                NumeroTel(str);
+                break;
+            case 3:
+                EmailValido(str);
+                break;      
+            case 4:
+                URLValida(str);
+                break;    
+            default:
+                break;
+        }
+        
         //CPFValidacao(str);
         //NumeroTel(str);
         //EmailValido(str);
-        URLValida(str);
-    }
+        //URLValida(str);
 
     return 0;
 }
